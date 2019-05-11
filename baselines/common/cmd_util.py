6e5bfc3b5d76de239bbdda86a6857b5569d9ea58
ga63zoc <maximilian.stadler@tum.de>
2019-05-11 13:01:40 +0200

    debugging

--- baselines/common/cmd_util.py
+++ baselines/common/cmd_util.py
@@ -180,7 +180,7 @@ def common_arg_parser():
     parser.add_argument('--save_video_interval', help='Save video every x steps (0 = disabled)', default=0, type=int)
     parser.add_argument('--save_video_length', help='Length of recorded video. Default: 200', default=200, type=int)
     parser.add_argument('--play', default=False, action='store_true')
-    parser.add_argument('--without_actor_critic', default=False, type=bool, action='store_true',
+    parser.add_argument('--without_actor_critic', default=False, action='store_true',
                         help="flag for actor-critic-scheme")
     return parser
 
